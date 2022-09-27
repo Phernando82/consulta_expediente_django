02ff3b0c5d0dc7c4e3992ab69981b58e4eb7e61b
Fernando Valverde <nandovalverde@gmail.com>
2022-09-27 17:00:42 +0100

    Instalas as ferramentes de testes

---
+++ base/test_home.py
@@ -0,0 +1,9 @@
+from django.test import Client
+
+
+# Client serve para emular requisições HTTP
+
+
+def test_status_code(client: Client):
+    resp = client.get('/')
+    assert resp.status_code == 200
