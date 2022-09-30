2a87624341beda41d3a5605db6bac345cf38f593
Fernando Valverde <nandovalverde@gmail.com>
2022-09-30 11:37:56 +0100

    Landing Page

--- base/test_home.py
+++ base/test_home.py
@@ -1,9 +1,23 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+
+from consulta_expediente.django_assertions import assert_contains
 
 
-# Client serve para emular requisições HTTP
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Consulta Expediente</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Consulta Expediente</a>')
